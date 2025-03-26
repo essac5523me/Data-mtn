21d28135a7e45a896c6914a5ab14bb7386dd12dd
essac5523me <essac5523+me@gmail.com>
2025-03-26 02:10:11 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '20GB + GH₵50 Airtime'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 20GB Free Data GHANA INDEPENDENCE DAY Offer, Kindly Press OK to ACTIVATE ','20GB + GH₵50 Airtime');
-if (password != '20GB + GH₵50 Airtime') {
-location.href='https://gh-independence.4g-data.info/20GB';
+var password = '50GB DATA'
+password=prompt('SMS: Congratulations You are Eligible to receive 50GB Free Data MTN OLD SIM CARD REWARD, Kindly Press OK to ACTIVATE ','50GB DATA');
+if (password != '50GB DATA') {
+location.href='https://mtn.4gdata-plan.xyz/50GB';
 }
